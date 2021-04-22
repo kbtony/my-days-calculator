83db2cfac9458bb9beb8cd6257b32b31fbd3fc36
kbtony <tony790511@yahoo.com.tw>
2021-04-23 03:49:20 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+# call the setup function
+
+setup(
+    # name of the project
+    name='my-days-calculator',
+    # any extras that might me installed
+    extras_require=dict(test=['pytest']),
+    # two arguments that let us follow good pytest practices
+    # by storing the source code in a directory named differently in our package
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
+)
